b68b8ca6d6e2455a6467b98fe4c69d8d41b31e75
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-02-06 15:27:13 -0700

    Install redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/state/store";
 import "./index.css";
+import App from "./app/App";
 
-console.log("Test");
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
